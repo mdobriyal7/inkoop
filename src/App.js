a81e8a1f1185a755086a9e5d0d51e1fec060acc4
Manish Dobriyal <mdobriyal7@gmail.com>
2022-08-07 15:16:25 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const [data, setdata] = React.useState("");
+  const [counter, setcounter] = React.useState([]);
+
+  function handleinput(event) {
+    if (event.target.value < 0) {
+      setdata("Enter a Positive Value");
+    } else {
+      setdata("");
+    }
+  }
+
+  function valuehandler(event) {
+    let inpNo = event.target.value;
+    if (inpNo != 0) {
+      const value1 = Number(inpNo) + Number(2);
+      const value2 = Number(inpNo) + Number(4);
+      const value3 = Number(inpNo) + Number(6);
+      setcounter([value1, value2, value3]);
+    } else {
+      setcounter([]);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div style={{ margin: "0px", padding: "0px" }}>
+      <div style={{ position: "absolute", left: "40%", top: "150px" }}>
+        <input
+          id="inpfield"
+          type="number"
+          onChange={handleinput}
+          style={{
+            border: "2px solid black",
+            borderRadius: "4px",
+            height: "20px",
+          }}
+        />
+        <p>{data}</p>
+      </div>
+      <div style={{ position: "absolute", left: "40%", top: "300px" }}>
+        <input
+          id="inpfield"
+          type="number"
+          onChange={valuehandler}
+          style={{
+            border: "2px solid black",
+            borderRadius: "4px",
+            height: "20px",
+          }}
+        />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {counter[0]} {counter[1]} {counter[2]}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
